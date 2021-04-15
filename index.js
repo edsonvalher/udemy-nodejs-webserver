c929fc21c08a8a716d61bdcbfc83154d7e981260
Edson Valenzuela <edsonvalher@gmail.com>
2021-04-15 14:52:45 -0600

    hola mundo

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hola mundo")
+const http = require('http')
+
+const server = http.createServer((request, response) => {
+    response.write('Hola Mundo')
+    response.end()
+
+})
+
+server.listen(8080)
+console.log("escuchando...", 8080)
