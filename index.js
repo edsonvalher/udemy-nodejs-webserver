ba737d65918d43e2585de86b8ebe82ebad0a5b96
Edson Valenzuela <edsonvalher@gmail.com>
2021-04-16 09:40:08 -0600

    contenido estatico en html

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express')
 const app = express()
 
 const port = 8080
-app.get('/', (req, res) => {
-    res.send('Home page')
-})
+
+//middleware contenido estatico html
+app.use(express.static('public'))
+
 
 app.get('/hola-mundo', (req, res) => {
     res.send('Hola mundo en su ruta')
@@ -12,6 +13,6 @@ app.get('/hola-mundo', (req, res) => {
 
 
 app.get('/*', (req, res) => {
-    res.send('404 page not found')
+    res.sendFile(__dirname + '/public/404.html')
 })
 app.listen(port, () => { console.log(`app listening at http://localhost:${port}`) })
