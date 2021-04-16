fa1f86d7d01816727d3cd21172ace4d8ece73342
Edson Valenzuela <edsonvalher@gmail.com>
2021-04-16 09:13:45 -0600

    old

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ const http = require('http')
 
 const server = http.createServer((request, response) => {
 
-    response.setHeader('Content-Disposition', 'attachment; filename=lista.csv')
-    response.writeHead(200, { 'Content-Type': 'application/csv' })
+    //response.setHeader('Content-Disposition', 'attachment; filename=lista.csv')
+    //response.writeHead(200, { 'Content-Type': 'application/csv' })
 
-    response.write('id,nombre\n')
-    response.write('1,Maria\n')
-    response.write('2,Juan\n')
-    response.write('3,Pedro\n')
+
+
+    response.write('Hola Mundo')
     response.end()
 
 })
