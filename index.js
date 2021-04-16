787baf57665f84f32f88eff3b5214e7fc06c4ccc
Edson Valenzuela <edsonvalher@gmail.com>
2021-04-16 10:08:26 -0600

    trabajando con handlebar

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+
 const express = require('express')
 const app = express()
 
 const port = 8080
 
+app.set('view engine', 'hbs')
+
 //middleware contenido estatico html
 app.use(express.static('public'))
 
+app.get('/', (req, res) => {
+    res.render('home')
+})
 
 app.get('/hola-mundo', (req, res) => {
     res.send('Hola mundo en su ruta')
