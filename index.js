74fb3dccb71411af49622162bd6c07d3c5292a2d
Edson Valenzuela <edsonvalher@gmail.com>
2021-04-16 09:59:05 -0600

    se cargo plantilla de ejercicio

--- index.js
+++ index.js
@@ -12,7 +12,12 @@ app.get('/hola-mundo', (req, res) => {
 })
 
 
-app.get('/*', (req, res) => {
-    res.sendFile(__dirname + '/public/404.html')
+app.get('/elements', (req, res) => {
+    res.sendFile(__dirname + '/public/elements.html')
 })
+
+app.get('/generic', (req, res) => {
+    res.sendFile(__dirname + '/public/generic.html')
+})
+
 app.listen(port, () => { console.log(`app listening at http://localhost:${port}`) })
